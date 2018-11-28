03697de72f8991a70a2aeac5ac8735052569af49
Ashutosh Aggarwal <ashutoshaggarwal@Ashutoshs-MacBook-Pro.local>
2018-11-29 01:26:00 +0530

    webpack 3 version

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const autoprefixer = require('autoPrefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // resolve method helps us to generate absolute path in the end,
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: ''
+
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader', // babel is transpiler
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        // use take array of loaders
+        use: [
+          { 
+            loader: 'style-loader' 
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          }, // parses from right to left means it uses css loader firt then style loader
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: [
+                    "> 1%",
+                    "last 2 versions"
+                  ]
+                })
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+};
